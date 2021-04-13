9e3e688c0781922bc36b340d025206c8c36e4fa7
Усачев Дмитрий Дмитриевич <vizant88@gmail.com>
2021-04-13 13:05:37 +0300

    add negative tests

--- src/test/java/tddkata/CalcTest.java
+++ src/test/java/tddkata/CalcTest.java
@@ -17,10 +17,26 @@ class CalcTest {
   }
 
   @Test
+  void calcEmptyStringNegative() {
+    Calc calc = new Calc();
+    int actual = calc.sum("");
+    int expected = 1;
+    assertEquals(expected, actual);
+  }
+
+  @Test
   void calcOneParam() {
     Calc calc = new Calc();
     int actual = calc.sum("1");
     int expected = 1;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void calcOneParamNegative() {
+    Calc calc = new Calc();
+    int actual = calc.sum("1");
+    int expected = 2;
     assertEquals(expected, actual);
   }
 
@@ -31,4 +47,14 @@ class CalcTest {
     int expected = 3;
     assertEquals(expected, actual);
   }
+
+  @Test
+  void calcTwoParamNegative() {
+    Calc calc = new Calc();
+    int actual = calc.sum("1, 2");
+    int expected = 4;
+    assertEquals(expected, actual);
+  }
+
+
 }
