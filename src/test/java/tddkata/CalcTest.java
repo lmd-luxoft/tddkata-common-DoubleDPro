5d1baf25d756f3498a8923140b27bdc19afaebe4
Усачев Дмитрий Дмитриевич <vizant88@gmail.com>
2021-04-13 12:59:46 +0300

    add test for empty, one and two params

--- src/test/java/tddkata/CalcTest.java
+++ src/test/java/tddkata/CalcTest.java
@@ -1,15 +1,34 @@
 package tddkata;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Calendar;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CalcTest {
 
   @Test
-  void calc() {
+  void calcEmptyString() {
     Calc calc = new Calc();
     int actual = calc.sum("");
     int expected = 0;
-    Assertions.assertEquals(expected, actual);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void calcOneParam() {
+    Calc calc = new Calc();
+    int actual = calc.sum("1");
+    int expected = 1;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void calcTwoParam() {
+    Calc calc = new Calc();
+    int actual = calc.sum("1, 2");
+    int expected = 3;
+    assertEquals(expected, actual);
   }
 }
