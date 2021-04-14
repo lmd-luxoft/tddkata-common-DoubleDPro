daa09b3f821a209827216c5dd3da75937c9a665e
Усачев Дмитрий Дмитриевич <vizant88@gmail.com>
2021-04-14 11:09:53 +0300

    add optional delimiter method

--- src/main/java/tddkata/Calc.java
+++ src/main/java/tddkata/Calc.java
@@ -3,8 +3,19 @@ package tddkata;
 public class Calc {
 
   public int sum(String expression) {
+    String currentDelimiter = ",|\n";
     if (expression != null && !expression.isEmpty()) {
-      String[] numbers = expression.split(",|\n");
+      if (expression.startsWith("//")) {
+        if (expression.contains("\n")) {
+          currentDelimiter = expression.substring(2, expression.indexOf("\n"));
+          expression = expression.substring(expression.indexOf("\n") + 1);
+        } else {
+          return -1;
+        }
+
+      }
+
+      String[] numbers = expression.split(currentDelimiter);
       int[] digits = new int[numbers.length];
       for (int i = 0; i < numbers.length; i++) {
         try {
